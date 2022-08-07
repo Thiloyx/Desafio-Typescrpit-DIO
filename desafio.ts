58236ee5bfe384e2810f5ea807f59002a6e20c45
Thiloyx <102920404+Thiloyx@users.noreply.github.com>
2022-08-06 23:12:36 -0300

    Resoluções dos desafios do módulo de Typescript

---
+++ desafio.ts
@@ -0,0 +1,24 @@
+// Exemplo 1
+const funcionario: {codigo: number, nome: string} = {
+    codigo: 10,
+    nome: 'joao'
+};
+
+
+// Exemplo 2
+const funcionario2 = {
+    codigo: 10,
+    nome: 'João'
+};
+
+// Exemplo 3
+interface Funcionario {
+    codigo: number,
+    nome: string
+};
+
+
+const funcionarioObj: Funcionario = {
+    codigo: 10,
+    nome: 'João'
+}
